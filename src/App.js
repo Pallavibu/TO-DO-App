7a48b9009d2074fc3eea4bd9e4ac1109f4a760ac
Pallavibu <pallavibu44@gmail.com>
2024-06-15 23:47:28 +0530

    giving style to the project page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Todoapp from './Components/Todoapp';
 function App() {
   return (
     <div className=" bg-lime-100 min-h-screen">
-    <h1 className='font-bold text-3xl flex justify-center py-5'>Small Projects on Reactjs</h1>
+    <h1 className='font-bold text-3xl flex justify-center py-5 px-2'>Small Projects on Reactjs</h1>
     <Counter/>
     <Todoapp/>
     </div>
